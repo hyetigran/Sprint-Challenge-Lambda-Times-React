5752511be5b10c714cc4902a0bcd9105c8b730b1
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-07 12:49:10 +0400

    completed tabs and cards components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -5,14 +5,15 @@ const Tab = props => {
 	/* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+	const isMatch = props.tab === props.selectedTab;
+	const activeTab = isMatch ? `tab .active-tab` : `tab`;
+
+	const onTabChange = event => {
+		props.selectTabHandler(props.tab);
+	};
+
 	return (
-		<div
-			className="tab"
-			onClick={() => {
-				/* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-			}}
-		>
+		<div className={activeTab} onClick onClick={onTabChange}>
 			{props.tab.toUpperCase()}
 		</div>
 	);
