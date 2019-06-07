d3138d4aadcaad2eb9f6255561baa528e70dbd59
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-06-07 13:19:24 +0400

    refactored styles in TopBar

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,96 @@
 import React from 'react';
-
-// Refactor this component to use styled components and not classNames. 
+import styled from 'styled-components';
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
+const TopBarDiv = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: none;
+	flex-direction: row;
+	position: fixed;
+	height: 44px;
+	background-color: #333;
+`;
+const TopBarContainerDiv = styled.div`
+	width: 100%;
+	display: flex;
+	justify-content: none;
+	align-items: none;
+	flex-direction: row;
+	color: #fff;
+	letter-spacing: 1px;
+	padding: 0 10px;
+`;
+const TopBarContainerDivLeft = styled.div`
+	display: flex;
+	justify-content: none;
+	align-items: center;
+	flex-direction: row;
+	flex: 1;
+	font-size: 11px;
+
+	span {
+		cursor: pointer;
+		margin-right: 25%;
+		font-weight: bold;
+	}
+`;
+const TopBarContainerCenterDiv = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+	flex: 3;
+	font-size: 9px;
+
+	span {
+		cursor: pointer;
+		margin-right: 5%;
+		&:last-child {
+			margin-right: 0;
+		}
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+`;
+const TopBarContainerRightDiv = styled.div`
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	flex-direction: row;
+	flex: 1;
+	font-size: 11px;
+	font-weight: bold;
+
+	span {
+		cursor: pointer;
+	}
+`;
+
 const TopBar = () => {
-  return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+	return (
+		<TopBarDiv>
+			<TopBarContainerDiv>
+				<TopBarContainerDivLeft>
+					<span>TOPICS</span>
+					<span>SEARCH</span>
+				</TopBarContainerDivLeft>
+				<TopBarContainerCenterDiv>
+					<span>GENERAL</span>
+					<span>BROWNBAG</span>
+					<span>RANDOM</span>
+					<span>MUSIC</span>
+					<span>ANNOUNCEMENTS</span>
+				</TopBarContainerCenterDiv>
+				<TopBarContainerRightDiv>
+					<span>LOG IN</span>
+				</TopBarContainerRightDiv>
+			</TopBarContainerDiv>
+		</TopBarDiv>
+	);
+};
 
 export default TopBar;
